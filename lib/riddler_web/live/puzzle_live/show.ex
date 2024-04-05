c2e697e2c684f5ff60a176e82fdaf3da236ce52d
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-04-05 07:16:04 +0200

    style show and add svg function component

--- lib/riddler_web/live/puzzle_live/show.ex
+++ lib/riddler_web/live/puzzle_live/show.ex
@@ -18,4 +18,25 @@ defmodule RiddlerWeb.PuzzleLive.Show do
 
   defp page_title(:show), do: "Show Puzzle"
   defp page_title(:edit), do: "Edit Puzzle"
+
+  attr :x, :integer, required: true
+  attr :y, :integer, required: true
+  attr :alive, :boolean, default: false
+
+  defp rect(assigns) do
+    ~H"""
+    <rect
+      x={@x * 10}
+      y={@y * 10}
+      width="10"
+      height="10"
+      rx="2"
+      fill={fill_color(@alive)}
+      class="hover:fill-slate-400"
+    />
+    """
+  end
+
+  defp fill_color(true), do: "black"
+  defp fill_color(false), do: "white"
 end
